2389adf33a70ef79e69a3852c1576dc43361cdc8
Brice Decaestecker <brice.decaestecker@gmx.fr>
2024-10-13 20:49:11 +0200

    Allow to enter CLI from files navigation mode

--- rebase/rebase.ml
+++ rebase/rebase.ml
@@ -506,7 +506,7 @@ module App (Info : Rebase_info_external) :
     | Cli s, Left -> { model with mode = Cli (Editing_line.left s) }, []
     | Cli s, Right -> { model with mode = Cli (Editing_line.right s) }, []
     | Cli s, Enter -> update_cli_command (Editing_line.to_string s) model
-    | [%cross_match (Navigate, Move), Char ':'] ->
+    | [%cross_match (Navigate, Move, Navigate_files [%cross_any]), Char ':'] ->
       switch_mode (Cli (Editing_line.init ":")) model, []
     | [%cross_match
         (Rename [%cross_any], Cli [%cross_any], Navigate_files [%cross_any]), Esc] ->
